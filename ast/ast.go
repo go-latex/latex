2922b317644bfda9e55eb80273bddc989a52c3d0
Sebastien Binet <binet@cern.ch>
2020-04-25 23:48:51 +0200

    ast,token: add package-level documentation

--- ast/ast.go
+++ ast/ast.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Package ast declares the types used to represent syntax trees for
+// LaTeX documents.
 package ast // import "github.com/go-latex/latex/ast"
 
 import (
--- token/token.go
+++ token/token.go
@@ -2,9 +2,11 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:generate stringer -type Kind
-
+// Package token defines constants representing the lexical tokens of
+// LaTeX documents.
 package token // import "github.com/go-latex/latex/token"
+
+//go:generate stringer -type Kind
 
 import (
 	"go/token"
