01eae8bd88f2e2f9ce2e7d9d934152877109c790
Sebastien Binet <binet@cern.ch>
2020-12-11 16:40:12 +0100

    drawtex/drawimg: test multiple fonts

--- drawtex/drawimg/drawimg_test.go
+++ drawtex/drawimg/drawimg_test.go
@@ -9,8 +9,22 @@ import (
 	"io/ioutil"
 	"testing"
 
+	"github.com/go-fonts/latin-modern/lmroman12bold"
+	"github.com/go-fonts/latin-modern/lmroman12italic"
+	"github.com/go-fonts/latin-modern/lmroman12regular"
+	"github.com/go-fonts/liberation/liberationsansbold"
+	"github.com/go-fonts/liberation/liberationsansbolditalic"
+	"github.com/go-fonts/liberation/liberationsansitalic"
+	"github.com/go-fonts/liberation/liberationsansregular"
+	"github.com/go-fonts/stix/stix2math"
+	"github.com/go-fonts/stix/stix2textbold"
+	"github.com/go-fonts/stix/stix2textbolditalic"
+	"github.com/go-fonts/stix/stix2textitalic"
+	"github.com/go-fonts/stix/stix2textregular"
 	"github.com/go-latex/latex/drawtex/drawimg"
+	"github.com/go-latex/latex/font/ttf"
 	"github.com/go-latex/latex/mtex"
+	"golang.org/x/image/font/sfnt"
 )
 
 func TestRenderer(t *testing.T) {
@@ -28,10 +42,21 @@ func TestRenderer(t *testing.T) {
 		return raw
 	}
 
+	fonts := map[string]*ttf.Fonts{
+		"gofont":     nil,
+		"lmroman":    lmromanFonts(t),
+		"stix":       stixFonts(t),
+		"liberation": liberationFonts(t),
+	}
+
 	for _, tc := range []struct {
 		name string
 		expr string
 	}{
+		{
+			name: "func",
+			expr: `$f(x)=ax+b$`,
+		},
 		{
 			name: "sqrt",
 			expr: `$\sqrt{x}$`,
@@ -46,20 +71,120 @@ func TestRenderer(t *testing.T) {
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
-			out := new(bytes.Buffer)
-			dst := drawimg.NewRenderer(out)
-			err := mtex.Render(dst, tc.expr, size, dpi)
-			if err != nil {
-				t.Fatalf("could not render expression %q: %+v", tc.expr, err)
-			}
+			for _, font := range []string{
+				"gofont",
+				"lmroman",
+				"stix",
+				"liberation",
+			} {
+				t.Run(font, func(t *testing.T) {
+					out := new(bytes.Buffer)
+					dst := drawimg.NewRenderer(out)
+					err := mtex.Render(dst, tc.expr, size, dpi, fonts[font])
+					if err != nil {
+						t.Fatalf("could not render expression %q: %+v", tc.expr, err)
+					}
 
-			if got, want := out.Bytes(), load(tc.name); !bytes.Equal(got, want) {
-				err := ioutil.WriteFile("testdata/"+tc.name+".png", got, 0644)
-				if err != nil {
-					t.Fatalf("could not create output file: %+v", err)
-				}
-				t.Fatal("files differ")
+					name := font + "_" + tc.name
+					if got, want := out.Bytes(), load(name); !bytes.Equal(got, want) {
+						err := ioutil.WriteFile("testdata/"+name+".png", got, 0644)
+						if err != nil {
+							t.Fatalf("could not create output file: %+v", err)
+						}
+						t.Fatal("files differ")
+					}
+				})
 			}
 		})
+	}
+}
+
+func lmromanFonts(t *testing.T) *ttf.Fonts {
+	rm, err := sfnt.Parse(lmroman12regular.TTF)
+	if err != nil {
+		t.Fatalf("could not parse fonts: %+v", err)
+	}
+
+	it, err := sfnt.Parse(lmroman12italic.TTF)
+	if err != nil {
+		t.Fatalf("could not parse fonts: %+v", err)
+	}
+
+	bf, err := sfnt.Parse(lmroman12bold.TTF)
+	if err != nil {
+		t.Fatalf("could not parse fonts: %+v", err)
+	}
+
+	return &ttf.Fonts{
+		Default: rm,
+		Rm:      rm,
+		It:      it,
+		Bf:      bf,
+		BfIt:    nil,
+	}
+}
+
+func stixFonts(t *testing.T) *ttf.Fonts {
+	rm, err := sfnt.Parse(stix2math.TTF)
+	if err != nil {
+		t.Fatalf("could not parse fonts: %+v", err)
+	}
+
+	def, err := sfnt.Parse(stix2textregular.TTF)
+	if err != nil {
+		t.Fatalf("could not parse fonts: %+v", err)
+	}
+
+	it, err := sfnt.Parse(stix2textitalic.TTF)
+	if err != nil {
+		t.Fatalf("could not parse fonts: %+v", err)
+	}
+
+	bf, err := sfnt.Parse(stix2textbold.TTF)
+	if err != nil {
+		t.Fatalf("could not parse fonts: %+v", err)
+	}
+
+	bfit, err := sfnt.Parse(stix2textbolditalic.TTF)
+	if err != nil {
+		t.Fatalf("could not parse fonts: %+v", err)
+	}
+
+	return &ttf.Fonts{
+		Default: def,
+		Rm:      rm,
+		It:      it,
+		Bf:      bf,
+		BfIt:    bfit,
+	}
+}
+
+func liberationFonts(t *testing.T) *ttf.Fonts {
+	rm, err := sfnt.Parse(liberationsansregular.TTF)
+	if err != nil {
+		t.Fatalf("could not parse fonts: %+v", err)
+	}
+
+	it, err := sfnt.Parse(liberationsansitalic.TTF)
+	if err != nil {
+		t.Fatalf("could not parse fonts: %+v", err)
+	}
+
+	bf, err := sfnt.Parse(liberationsansbold.TTF)
+	if err != nil {
+		t.Fatalf("could not parse fonts: %+v", err)
+	}
+
+	bfit, err := sfnt.Parse(liberationsansbolditalic.TTF)
+	if err != nil {
+		t.Fatalf("could not parse fonts: %+v", err)
+	}
+
+	return &ttf.Fonts{
+		Default: rm,
+		Rm:      rm,
+		It:      it,
+		Bf:      bf,
+		BfIt:    bfit,
 	}
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.13
 require (
 	github.com/fogleman/gg v1.3.0
 	github.com/go-fonts/dejavu v0.1.0
+	github.com/go-fonts/latin-modern v0.2.0
+	github.com/go-fonts/liberation v0.1.1
+	github.com/go-fonts/stix v0.1.0
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
 	github.com/jung-kurt/gofpdf v1.16.2
 	golang.org/x/image v0.0.0-20201208152932-35266b937fa6
