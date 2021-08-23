f69c58ca712c42ba0952948cefcb3d42c4d5bc29
Sebastien Binet <binet@cern.ch>
2021-08-23 10:35:12 +0200

    all: apply gofmt simplify

--- internal/fakebackend/fakebackend_xheight_gen.go
+++ internal/fakebackend/fakebackend_xheight_gen.go
@@ -8,9 +8,9 @@ func init() {
 		xhKey{"default", 12, 72}: 6.5625,
 		xhKey{"regular", 10, 72}: 5.46875,
 		xhKey{"regular", 12, 72}: 6.5625,
-		xhKey{"rm", 10, 72}: 5.46875,
-		xhKey{"rm", 12, 72}: 6.5625,
-		xhKey{"it", 10, 72}: 5.46875,
-		xhKey{"it", 12, 72}: 6.5625,
+		xhKey{"rm", 10, 72}:      5.46875,
+		xhKey{"rm", 12, 72}:      6.5625,
+		xhKey{"it", 10, 72}:      5.46875,
+		xhKey{"it", 12, 72}:      6.5625,
 	}
 }
